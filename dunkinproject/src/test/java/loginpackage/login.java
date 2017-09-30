2e7265fe31af8ab3f7b0461218105a86a3662b57
Brahmbhatt P <Brahmbhatt P@DESKTOP-SQ9JDBQ>
2017-09-30 10:18:19 -0500

    updating by pb

---
+++ dunkinproject/src/test/java/loginpackage/login.java
@@ -0,0 +1,11 @@
+package loginpackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+
+	public static void main(String[] args) {
+		ChromeDriver driver= new ChromeDriver();
+driver.get("https://www.facebook.com/");
+}
+}
