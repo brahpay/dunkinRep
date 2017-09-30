dc8ff983d96ffa575eab8fc1311f637a56fde313
Brahmbhatt P <Brahmbhatt P@DESKTOP-SQ9JDBQ>
2017-09-30 10:50:40 -0500

    test

--- dunkinproject/src/test/java/loginpackage/login.java
+++ dunkinproject/src/test/java/loginpackage/login.java
@@ -1,12 +1,20 @@
 package loginpackage;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class login {
 
 	public static void main(String[] args) {
 		ChromeDriver driver= new ChromeDriver();
+		Logger log=Logger.getLogger("devpinoylogger");
 driver.get("https://www.facebook.com/");
+log.debug("Facebook Application is opened");
+String a=driver.getTitle();
+log.debug("Facebook Title is printed");
+System.out.println(a);
+System.out.println("welcome");
 driver.close();
+
 }
 }
